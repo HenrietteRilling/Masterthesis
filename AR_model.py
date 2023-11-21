874ddd37052112e89322ca9912977bb0158e57cc
Henriette Rilling <henriette.rilling@posteo.de>
2023-11-21 13:38:48 +0100

    comments

--- AR_model.py
+++ AR_model.py
@@ -19,30 +19,30 @@ class samplemodel(torch.nn.Module):
     
   
   def forward(self, inputs, labels):
-      #generate first prediciton from the input 
+      # import pdb
+      # pdb.set_trace()
+      #generate first prediciton from the input
       preds=self.model(inputs)
-      #initialize first prediction
+      #initialize first prediction, we only take the last value as it is the next timestep in the future
       result_tensor=torch.unsqueeze(preds[:,-1,:],1)
 
       ############################
       #cycle through all time steps up until the maximal gap length/prediction horizon feeding predictions back
       n=0
-      #import pdb
-      #pdb.set_trace()
       while n<labels.shape[1]-1: #-1 as for last prediction there is no more precipitation value in the features 
           input_for_this_step = inputs[:,1:,:]
-          #add prcp observation of the respective timestep as input, prcp is "stored in the input
+          #add prcp observation of the respective timestep as input, prcp is "stored in the labels, we need to add a dimesion after extracting values
           preds_for_this_step=torch.cat([preds[:,-1:,:], torch.unsqueeze(labels[:,n,1:],1)],2)
           #check for missing values  in the input
           nan_mask=torch.isnan(input_for_this_step)
           if torch.any(nan_mask):
               #Replace missing values with predictions from timestep before
               input_for_this_step[nan_mask]=preds_for_this_step[nan_mask]  
-          #concat observation and predictions
+          #concatenate observation and predictions
           modelinput=torch.cat([input_for_this_step,preds_for_this_step],1)
+          #generate new prediction
           preds=self.model(modelinput)
           result_tensor=torch.cat([result_tensor,torch.unsqueeze(preds[:,-1,:],1)],1)
-          #
           n+=1
       return(result_tensor)     
   
