f212125aa4430967fdc41c74796f612b19d7ce85
Henriette Rilling <henriette.rilling@posteo.de>
2023-12-08 16:36:27 +0100

    added Persistency index

--- AR_model/metrics.py
+++ AR_model/metrics.py
@@ -20,11 +20,30 @@ def rmse(y, y_hat, savepath):
     if savepath != None:
         #save in logfile
         with open(savepath, 'a') as file:
-            file.write(f'RMSE: {rmse}')
-    return rmse
+            file.write(f'{rmse};')
+    else: return rmse
 
 
 # =============================================================================
 # Add Persistence index
 # =============================================================================
     
+
+def PI(y, ymin1, y_hat, savepath):
+    '''
+    Persistence Index
+    compares the sum of squared error to the error that would occur if the value was forecast as the previous observed value.
+    y = true observations
+    ymin1 = true observations, shifted by one time step
+    y_hat = predictions
+    '''
+    import pdb
+    pdb.set_trace()
+    SSE_pred=np.sum((y-y_hat)**2)
+    SSE_prev=np.sum((y-ymin1)**2)
+    PI=1-(SSE_pred/SSE_prev) 
+    if savepath != None:
+        #save in logfile
+        with open(savepath, 'a') as file:
+            file.write(f'{PI}\n')
+    else: return PI
