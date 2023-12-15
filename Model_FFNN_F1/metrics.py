0909a9da109963a666835c81cee15a3026c98e51
Henriette Rilling <henriette.rilling@posteo.de>
2023-12-15 10:03:20 +0100

    FFNN with one input feature, used for first simulation of imputation

---
+++ Model_FFNN_F1/metrics.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Dec  1 15:38:04 2023
+
+@author: Henriette
+"""
+import os
+import numpy as np
+
+
+def rmse(y, y_hat, savepath):
+    '''
+    Function for calculating root mean squared error (RMSE)
+    y= array, true values
+    y_hat= array, predictions
+    '''
+    #calculate RMSE
+    mse=np.mean((y-y_hat)**2)
+    rmse=np.sqrt(mse)
+    if savepath != None:
+        #save in logfile
+        with open(savepath, 'a') as file:
+            file.write(f'{rmse};')
+    else: return rmse
+
+
+# =============================================================================
+# Add Persistence index
+# =============================================================================
+    
+
+def PI(y, ymin1, y_hat, savepath):
+    '''
+    Persistence Index
+    compares the sum of squared error to the error that would occur if the value was forecast as the previous observed value.
+    y = true observations
+    ymin1 = true observations, shifted by one time step, onlyt first value is needed
+    y_hat = predictions
+    '''
+    #crate array only containing first observation
+    ymin1_0=np.tile(ymin1[:,:1],ymin1.shape[1])
+    SSE_pred=np.sum((y-y_hat)**2)
+    SSE_prev=np.sum((y-ymin1_0)**2)
+    PI=1-(SSE_pred/SSE_prev) 
+    if savepath != None:
+        #save in logfile
+        with open(savepath, 'a') as file:
+            file.write(f'{PI}\n')
+    else: return PI
+    
+    
