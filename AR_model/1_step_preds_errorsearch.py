70bf07adff9c8e42822959f13e193b8c3656004e
Henriette Rilling <henriette.rilling@posteo.de>
2024-01-19 16:07:56 +0100

    adjusted layout of plots for pdf

--- AR_model/1_step_preds_errorsearch.py
+++ AR_model/1_step_preds_errorsearch.py
@@ -64,11 +64,43 @@ for config in config_list[:1]:
     test_id=config[-2]
     test_prcp=config[-1]   
     #one figure per configuration
+
+X_test_rain_array=X_test[[test_prcp]].to_numpy()
+
+no_prcp=False
+if no_prcp:
+    respath=r'C:\Users\henri\Desktop\LSTM_prelim_2'
+    configpath=os.path.join(respath, 'configs.csv')
+    #Read csv file with model configurations
+    with open(configpath, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file)
+        
+        # Convert the CSV rows to a list
+        config_list = list(csv_reader)
+    
+    
+    for config in config_list[:1]:
+        #get path of pkl file for current model configuration
+        pred_pkl_path=os.path.join(respath, f'{os.path.basename(config[0])}.pkl')
+        #Read pickle
+        if os.path.exists(pred_pkl_path):
+            axidx+=1
+            with open(pred_pkl_path, 'rb') as pickle_file:
+                data = pickle.load(pickle_file)
+        else: continue 
+        
+        #Extract data
+        pred_list=data[0]
+        X_test=data[1]
+        
+        #Get id of test station and precipitation station
+        test_id=config[-2]  
+        #one figure per configuration
     
 preds=pred_list[0]
 #calculate error of 1-step prediction model results
 X_test_array=X_test[[test_id]].to_numpy()
-X_test_rain_array=X_test[[test_prcp]].to_numpy()
+
 #negative error=oversetimation
 error_preds=X_test_array[1:]-preds[1:,:1]
 error_prevobs=X_test_array[1:]-X_test_array[:-1]
@@ -138,7 +170,7 @@ plt.legend()
 plt.xlabel('Observed water level [m]')
 plt.ylabel('Residual')
 
-#residuals agains precipitation
+#residuals against precipitation
 plt.figure()
 plt.scatter(X_test_rain_array[1:], error_preds, s=msize, c='darkorange',label='preds')
 plt.scatter(X_test_rain_array[1:], error_prevobs, s=msize, c='green',label='prevobs')
@@ -188,7 +220,7 @@ ax1.plot(dates[1:],error_prevobs, linestyle='None', marker='.', ms=msize, color=
 # ax1.legend()
 ax1.set_ylabel('Residual')
 
-ax2.plot(dates[1:], X_test_array[1:], color='blue', label='Observation', linestyle='-', marker='.', ms=msize)
+ax2.plot(dates[1:], X_test_array[1:], color='blue', label='Observation', linestyle='None', marker='.', ms=msize)
 ax2.plot(dates[1:], preds[1:,:1], color='darkorange', label='Predictions', linestyle='None', marker='.', ms=msize)
 ax2.plot(dates[1:], X_test_array[:-1], color='green', label='Previous observation', linestyle='None', marker='.', ms=msize)
 ax2.set_xlabel('Date')
@@ -209,8 +241,10 @@ ax3.set_yticklabels([f'{abs(y):.0f}' for y in yticks])
 ax3.set_ylabel('Precipitation [mm/h]')
 
 
-start_date=pd.to_datetime('2022-09-05')
-end_date=pd.to_datetime('2022-09-17')
+
+#zoomed plot
+start_date=pd.to_datetime('2022-06-04')
+end_date=pd.to_datetime('2022-06-13')
 
 
 fig, ax=plt.subplots(2, 1, sharex=True)
@@ -223,7 +257,7 @@ ax1.plot(dates[1:],error_prevobs, linestyle='None', marker='.', ms=msize, color=
 ax1.set_ylabel('Residual')
 ax1.set_xlim(start_date, end_date)
 
-ax2.plot(dates[1:], X_test_array[1:], color='blue', label='Observation', linestyle='-', marker='.', ms=msize)
+ax2.plot(dates[1:], X_test_array[1:], color='blue', label='Observation', linestyle='None', marker='.', ms=msize)
 ax2.plot(dates[1:], preds[1:,:1], color='darkorange', label='Predictions', linestyle='None', marker='.', ms=msize)
 ax2.plot(dates[1:], X_test_array[:-1], color='green', label='Previous observation', linestyle='None', marker='.', ms=msize)
 ax2.set_xlabel('Date')
@@ -231,7 +265,7 @@ ax2.set_ylabel('WL [m]')
 ax2.set_xlim(start_date, end_date)
 ax2.set_ylim(49.6, 50.25 )
 plt.xticks(rotation=25)
-fig.legend(loc='upper center', ncol=3, fontsize='medium', frameon=False)
+fig.legend(loc='upper center', ncol=3, fontsize='medium', frameon=False, markerscale=6)
 
 
 ax3 =ax1.twinx()
@@ -249,3 +283,50 @@ ax3.set_ylabel('Precipitation [mm/h]')
 
 
 
+
+#resdiuals over time with precipitation
+fig, ax =plt.subplots(1,1,figsize=cm2inch((15,7)))
+ax.plot(dates[1:],error_preds, label='Prediction', color='darkorange', linestyle='None', marker='.', ms=msize)
+ax.plot(dates[1:],error_prevobs, label='Previous observation',color='green', linestyle='None', marker='.', ms=msize)
+# ax.legend(loc='lower left', fontsize='medium')
+ax.set_ylabel('Residual [m]', fontsize='large')
+ax.set_xlabel('Date', fontsize='large')
+
+#adjust xticks
+locator = mdates.AutoDateLocator(minticks=3, maxticks=7)
+formatter = mdates.ConciseDateFormatter(locator)
+ax.xaxis.set_major_locator(locator)
+ax.xaxis.set_major_formatter(formatter)
+ax.tick_params('x', labelsize='medium')
+
+ax1=ax.twinx()
+ax1.bar(dates[1:], -X_test_rain_array[1:, 0], width=0.05, color='lightsteelblue',edgecolor='black') #alternative: cornflowerblue
+# Set the y-axis tick labels to display without decimal precision
+ax1.tick_params('y', labelsize='medium')
+ax1.set_ylim(-40, 0)
+
+# Invert the tick labels on the second y-axis such that they are displayed positive
+yticks = ax1.get_yticks()
+ax1.set_yticks(yticks)
+#make yticks label positive and without decimals
+ax1.set_yticklabels([f'{abs(y):.0f}' for y in yticks])
+ax1.set_ylabel('Precipitation [mm/h]', rotation=-90, labelpad=11, fontsize='large')
+fig.legend(loc='upper center', ncol=2, fontsize='medium', frameon=False, markerscale=5)
+plt.subplots_adjust(left= 0.02, bottom=0.02,right=1.0, top=0.9, hspace=0.2)
+#adjust space tight layout is taking in windows canva, neede that legend on top and label in bottom are shown. 
+plt.tight_layout(rect=[0.02, 0.02 ,1.0, 0.9],pad=0.3) #rect: [left, bottom, right, top]
+plt.savefig(os.path.join(respath, 'LSTM_TH1_IH1_error.png'), dpi=600)
+
+
+
+#residuals against precipitation
+plt.figure(figsize=cm2inch((15,7)))
+plt.scatter(X_test_rain_array[1:], error_preds, s=msize, c='darkorange',label='Prediction')
+plt.scatter(X_test_rain_array[1:], error_prevobs, s=msize, c='green',label='Previous observation')
+plt.legend(fontsize='medium', markerscale=3)
+plt.xlabel('Precipitation [mm/h]', fontsize='large')
+plt.ylabel('Residual [m]', fontsize='large')
+plt.xticks(fontsize='medium')
+plt.yticks(fontsize='medium')
+plt.tight_layout()
+plt.savefig(os.path.join(respath, 'LSTM_TH1_IH1_res_scatter.png'), dpi=600)
