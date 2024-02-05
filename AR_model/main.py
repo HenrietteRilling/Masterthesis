a5fcc2dc4e6209849901d984bfd512e2fa159e42
Henriette Rilling <henriette.rilling@posteo.de>
2024-02-05 17:18:59 +0100

    added option to include other water level

--- AR_model/main.py
+++ AR_model/main.py
@@ -7,6 +7,7 @@ Created on Thu Nov  2 10:09:19 2023
 
 import json
 import csv
+import pandas as pd
 
 from utils import load_data
 from run_sim_LSTM import run_LSTM
@@ -17,12 +18,29 @@ if __name__ == '__main__':
     #read configuration
     with open('run_config_LSTM.json', 'r', encoding='utf-8') as f:
         config = json.load(f)
+
     # respath_list=[r'./Results/lstm_W_10_B_100_N_25_L_1']
     configs_tested=[]
     #train models
+    #set-up if there is only 1 or 2 input features (WL and precipitation )
+    import pdb
+    pdb.set_trace()
     for model in config['models']:
         #load data
         X, test_id=load_data(config['test_station'], config['prcp_station'])
+        
+        #if another WL station is added as additional input, load data as well
+        if config['other_station'] != 'None':
+            #load data that is added as input feature to imputation
+            X2, test_id2=load_data(config['other_station'], 'None')
+            #choose overlapping timeperiod, such that time-frames are aligned
+            X=X[config['train_period'][0]:config['test_period'][1]]
+            X2=X2[config['train_period'][0]:config['test_period'][1]]
+            #merge data
+            X=pd.concat([X, X2], axis=1)
+            #free memory
+            del X2            
+            
         if model=='lstm':
             for window in config['window_size']:
                 for batch_size in config['batch_size']:
@@ -38,17 +56,5 @@ if __name__ == '__main__':
                                       config['epochs'], batch_size, config['n_models'],
                                       neurons, nr_layers, window
                                       )
-                            configs_tested.append([respath, model, window, batch_size, neurons, nr_layers, test_id, config['prcp_station']])
+                            configs_tested.append([respath, model, window, batch_size, neurons, nr_layers, test_id, config['prcp_station'], config['other_station']])
                             print('Finished config')
-
-
-    #Save configs in csv file
-    csv_file_path = './Results/configs.csv'    
-    with open(csv_file_path, 'w', newline='') as csv_file:
-        csv_writer = csv.writer(csv_file)
-        [csv_writer.writerow(row) for row in configs_tested]
-
-
-
-    
-
